8ec0a1c76c41e5eea73125da2491ae277dec5142
PZJ <11253705+pzjpzjpzj@user.noreply.gitee.com>
2024-03-07 13:11:59 +0800

    探索功能

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module traversal-share
 
 go 1.16
 
-require github.com/pion/stun v0.6.1
+require github.com/ccding/go-stun/stun v0.0.0-20200514191101-4dc67bcdb029
--- main.go
+++ main.go
@@ -5,45 +5,115 @@ package main
 
 import (
 	"fmt"
-	"github.com/pion/stun"
+	"net"
+	"time"
+
+	"github.com/ccding/go-stun/stun" // 引入 go-stun 包，用于与 stun 服务器交互
 )
 
-// stunRequest 发送 STUN 请求到指定的 STUN 服务器，并输出解析后的 XOR-MAPPED-ADDRESS。
-func stunRequest(serverURL string) {
-	// 解析 STUN URI
-	u, err := stun.ParseURI("stun:" + serverURL)
+const (
+	stunServer = "stun.radiojar.com:3478" // stun 服务器的地址，可以换成其他可用的地址
+)
+
+// Client 客户端结构体，包含名称，本地地址，公网地址和套接字
+type Client struct {
+	name       string
+	localAddr  *net.UDPAddr
+	publicAddr *net.UDPAddr
+	conn       *net.UDPConn
+}
+
+// NewClient 创建一个客户端，绑定一个本地 udp 端口，并向 stun 服务器发送请求，获取自己的公网地址
+func NewClient(name string) (*Client, error) {
+	c := &Client{name: name}
+	var err error
+	c.conn, err = net.ListenUDP("udp4", nil) // 随机分配一个本地 udp 端口
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-
-	// 创建到 STUN 服务器的连接
-	c, err := stun.DialURI(u, &stun.DialConfig{})
+	c.localAddr = c.conn.LocalAddr().(*net.UDPAddr)
+	fmt.Printf("%s: 本地地址: %s\n", c.name, c.localAddr)
+	// 创建一个 stun 客户端，指定 stun 服务器的地址
+	stunClient := stun.NewClient()
+	stunClient.SetServerAddr(stunServer)
+	// 向 stun 服务器发送请求，获取自己的 nat 类型和公网地址
+	nat, host, err := stunClient.Discover()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+	fmt.Printf("%s: NAT 类型: %s\n", c.name, nat)
+	fmt.Printf("%s: 公网地址: %s\n", c.name, host)
+	// 解析公网地址，得到公网 ip 和端口
+	ip := net.IPv4(host.IP()[0], host.IP()[1], host.IP()[2], host.IP()[3])
+	port := int(host.Port())
+	c.publicAddr = &net.UDPAddr{
+		IP:   ip,
+		Port: port,
+	}
+	return c, nil
+}
 
-	// 构建带有随机事务 ID 的绑定请求
-	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
-
-	// 发送请求到 STUN 服务器，等待响应消息
-	if err := c.Do(message, func(res stun.Event) {
-		if res.Error != nil {
-			panic(res.Error)
+// 从套接字中读取数据，并打印出来
+func (c *Client) read() {
+	for {
+		data := make([]byte, 1024)
+		n, addr, err := c.conn.ReadFromUDP(data)
+		if err != nil {
+			fmt.Printf("%s: 读取数据错误: %v\n", c.name, err)
+			continue
 		}
+		fmt.Printf("%s: 从 %s 收到数据: %s\n", c.name, addr, data[:n])
+	}
+}
 
-		// 从消息中解码 XOR-MAPPED-ADDRESS 属性
-		var xorAddr stun.XORMappedAddress
-		if err := xorAddr.GetFrom(res.Message); err != nil {
-			panic(err)
+// 向目标地址发送数据，每隔一秒发送一次，直到程序退出
+func (c *Client) write(target *net.UDPAddr) {
+	for {
+		// 向目标地址发送一条问候语，包含自己的名称
+		data := []byte(fmt.Sprintf("Hello, I am %s", c.name))
+		_, err := c.conn.WriteToUDP(data, target)
+		if err != nil {
+			fmt.Printf("%s: 发送数据错误: %v\n", c.name, err)
+		} else {
+			fmt.Printf("%s: 向 %s 发送数据: %s\n", c.name, target, data)
 		}
-		fmt.Println(xorAddr.IP, ":", xorAddr.Port)
-	}); err != nil {
-		panic(err)
+		time.Sleep(time.Second) // 暂停一秒
 	}
 }
 
 func main() {
-	// 向两个不同的 STUN 服务器发送请求并输出结果
-	stunRequest("stun.radiojar.com:3478")
-	stunRequest("stun.miwifi.com:3478")
+	// 创建两个客户端，分别命名为 A 和 B
+	clientA, err := NewClient("A")
+	if err != nil {
+		fmt.Println("创建客户端 A 错误:", err)
+		return
+	}
+	clientB, err := NewClient("B")
+	if err != nil {
+		fmt.Println("创建客户端 B 错误:", err)
+		return
+	}
+	// 让两个客户端分别输入对方的公网 ip 和端口，作为目标地址
+	var ip string
+	var port int
+	fmt.Println("请输入客户端 A 的目标地址 (ip:port):")
+	fmt.Scanln(&ip, &port)
+	targetA := &net.UDPAddr{
+		IP:   net.ParseIP(ip),
+		Port: port,
+	}
+	fmt.Println("请输入客户端 B 的目标地址 (ip:port):")
+	fmt.Scanln(&ip, &port)
+	targetB := &net.UDPAddr{
+		IP:   net.ParseIP(ip),
+		Port: port,
+	}
+	// 让两个客户端同时开始读写数据
+	go clientA.read()
+	go clientB.read()
+	go clientA.write(targetA)
+	go clientB.write(targetB)
+	// 主程序阻塞等待，直到按下回车键退出
+	fmt.Println("按下回车键退出...")
+	fmt.Scanln()
 }
